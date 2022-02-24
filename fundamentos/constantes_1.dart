331f2894fae497bb2d7eda8ef84939f86b482c7a
lucasmlima13 <lucasmlima@gmail.com>
2022-02-24 08:20:16 -0300

    Constantes1 teste

--- fundamentos/constantes_1.dart
+++ fundamentos/constantes_1.dart
@@ -2,7 +2,13 @@ import 'dart:io';
 
 void main() {
   //Area da circunferência = PI * raio * raio
+  const PI = 3.1415;
 
-  var texto = stdin.readLineSync()!;
-  print("O valor digitado é: " + texto);
+  stdout.write("Informe o raio: ");
+  var entradaDoUsuario = stdin.readLineSync()!;
+  final double raio = double.parse(entradaDoUsuario);
+
+  var area = PI * raio * raio;
+
+  print("O valor do raio é: " + area.toString());
 }
